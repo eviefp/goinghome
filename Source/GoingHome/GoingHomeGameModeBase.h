78065d75895d04c3bfa6b08fe38ec9305e1e95b2
Vlad Ciobanu <admin@cvlad.info>
2016-11-21 03:14:28 +0200

    Initial commit

---
+++ Source/GoingHome/GoingHomeGameModeBase.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "GoingHomeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOINGHOME_API AGoingHomeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AGoingHomeGameModeBase();
+};
